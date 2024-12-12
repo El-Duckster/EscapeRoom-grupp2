f58ed291457341833e91bdcf3a40a58e288ec0c9
El-Duckster <144563447+El-Duckster@users.noreply.github.com>
2024-12-12 12:21:17 +0100

    Updates

---
+++ assets/main-fCjMOwFN.js
@@ -0,0 +1,2 @@
+import{f as o,r as g}from"./renderChallenges-UY7O01x_.js";const e=document.querySelector(".challenges__list");let l=[];const i=async()=>{const{success:s,data:a,error:n}=await o();if(!s){console.error("Failed to fetch challenges:",n),e.innerHTML="<li>Error loading challenges. Please try again later.</li>";return}l=a;const r=[...l].sort((t,c)=>c.rating-t.rating).slice(0,3);g(r,e)};i();
+//# sourceMappingURL=main-fCjMOwFN.js.map
